8e5ad1be9eefb7a1ff40990fb31dba8d16e6e424
Stephanie Patton <stephaniepatton@stephanies-air.attlocal.net>
2019-09-10 22:07:27 -0500

    day-2 completed ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -23,6 +23,43 @@ class Graph:
     def _unvisited_edges(self, vertex, visited):
         return [i for i in self.vertices.get(vertex) if i not in visited]
 
-        
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    def find_earliest(self, child):
+        verts = self.vertices
+
+        queue = [[child]]
+        visited = {child}
+        ancestries = []
+
+        for i in range(len(verts)):
+            try:
+                path = queue.pop(0)
+                visit = path[-1]
+                visited.add(visit)
+                branches = set(self._unvisited_edges(visit, visited))
+                if len(branches) == 0:
+                    ancestries.append(path)
+                else:
+                    for i in branches:
+                        queue.append(path + [i])
+            except IndexError:
+                longest_lineage = max([len(i) for i in ancestries])
+                earliest_ancestries = [
+                    i for i in ancestries if len(i) >= longest_lineage]
+                earliest_ancestor = min([i[-1] for i in earliest_ancestries])
+                break
+            except TypeError:
+                earliest_ancestor = -1
+                break
+
+        if earliest_ancestor == child:
+            earliest_ancestor = -1
+
+        return earliest_ancestor
+
+
+def earliest_ancestor(ancestors, child):
+    graph = Graph()
+    for j, k in ancestors:
+        graph.add_parents(j, k)
+
+    return graph.find_earliest(child)
